74cd69ce61339a6cf4001bc75a831a180b48d16d
ycarowr <ywrycaro@gmail.com>
2019-04-07 16:58:14 +0300

    Add Level Music

---
+++ Assets/Level1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1 : MonoBehaviour
+{
+    void Start()
+    {
+        if(MusicManager.Instance)
+            MusicManager.Instance.PlayMusicLevel();
+    }
+}
